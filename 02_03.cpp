989fa93ad6df8d3c0dba9b71d11e530342074ddc
Naveen Kushwaha <naveen17593@gmail.com>
2026-06-09 10:43:10 +0530

    A02 03

---
+++ 02_03.cpp
@@ -0,0 +1,17 @@
+/*
+Assignment : 02
+Topic : Basics of C++
+03 ||       Write a C++ program to calculate sum of two numbers.
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    c = a+b;
+    cout<<"Sum of "<<a<<" and "<<b<<" = "<<c;
+    return 0;
+}
