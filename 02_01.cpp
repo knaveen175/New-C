831d98664f6ec43d4b69a789c6fd122fabda6917
Naveen Kushwaha <naveen17593@gmail.com>
2026-06-09 10:42:31 +0530

    A02 01

---
+++ 02_01.cpp
@@ -0,0 +1,12 @@
+/*
+Assignment : 02
+Topic : Basics of C++
+01 ||       Write a C++ program to print Hello MySirG on the screen.
+
+*/
+#include<iostream>
+int main()
+{
+    std::cout<<"Hello MySirG";
+    return 0;
+}
