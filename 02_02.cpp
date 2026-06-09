29fed0012712439d6c2c212d933f7cbcab06e3bc
Naveen Kushwaha <naveen17593@gmail.com>
2026-06-09 10:42:50 +0530

    A02 02

---
+++ 02_02.cpp
@@ -0,0 +1,12 @@
+/*
+Assignment : 02
+Topic : Basics of C++
+02 ||       Write a C++ program to print Hello on the first line and MySirG on the second line using endl.
+
+*/
+#include<iostream>
+int main()
+{
+    std::cout<<"Hello"<<std::endl;std::cout<<"MySirG";
+    return 0;
+}
