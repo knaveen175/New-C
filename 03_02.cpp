f4a311b6fb589eaf8be1b2636b750d63dff2d84d
Naveen Kushwaha <naveen17593@gmail.com>
2026-06-10 09:01:53 +0530

    A03 02

---
+++ 03_02.cpp
@@ -0,0 +1,18 @@
+/*
+Assignment : 03
+Topic : Basics of C++
+02 ||       Write a C++ program to calculate the square of a number.
+
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter a Number: ";
+    cin>>a;
+    int sqr = a*a;
+    cout<<"Square = "<<sqr;
+    return 0;
+}
